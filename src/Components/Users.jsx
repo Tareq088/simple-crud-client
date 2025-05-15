b10193be4d7a8866946608242bab02bf74f36661
Tareq1203088 <trahmanbpdb19@gmail.com>
2025-05-15 09:32:36 +0600

    delete done

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -23,7 +23,7 @@ const Users = ({usersPromise}) => {
         .then(res=> res.json())
         .then(data => {
             console.log("dta after creating user in the db",data);
-                    
+                    //insertedId bujhbo kivabe: data er moddhe property thakee.
             if(data.insertedId){
                         // newUser e kono id nai. backend (mongodb) theke id ta asteche. tai _id tao dhukabo
                 newUser._id= data.insertedId;
@@ -32,9 +32,27 @@ const Users = ({usersPromise}) => {
             }
         })
     }
+     const handleDeleteUser =(id) =>{
+        console.log("delete the user", id)
+        fetch(`http://localhost:3000/users/${id}`,{
+            method:"DELETE",
+        })
+        .then(res=> res.json())
+        .then(data=>{
+            
+             //deletedCount bujhbo kivabe: data er moddhe property thakee.
+            if(data.deletedCount){
+                    // jei id er x e click korchi oita bade baki gula dekhabo
+                const remaininUsers = users.filter(user => user._id !== id );
+                setUsers(remaininUsers);
+                console.log("after delete", data);
+            }
+        })
+     }
 
     return (
         <div>
+            <h4>User: {users.length}</h4>
                     {/* add user */}
             <div>
                 <form onSubmit={handleUser} action="">
@@ -49,7 +67,10 @@ const Users = ({usersPromise}) => {
             <div>
                 {
                     users.map(user =>
-                        <p key={user._id}>{user.name} : {user.email}</p>)
+                        <p 
+                        key={user._id}>
+                        {user.name} : {user.email} 
+                        <button onClick={()=>handleDeleteUser(user._id)}>X</button></p>)
                 }
             </div>
         </div>
